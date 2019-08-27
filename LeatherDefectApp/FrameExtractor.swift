33103cd485854be75f9045cf7ea9985c664b7ce6
Andre Elandra <andreelandra@gmail.com>
2019-08-27 19:33:26 +0700

    Initial Commit

---
+++ LeatherDefectApp/FrameExtractor.swift
@@ -0,0 +1,15 @@
+//
+//  FrameExtractor.swift
+//  LeatherDefectApp
+//
+//  Created by Andre Elandra on 27/08/19.
+//  Copyright © 2019 Andre Elandra. All rights reserved.
+//
+
+import AVFoundation
+
+class FrameExtractor {
+    
+    
+    
+}
